a93393247ff1b0745690e897e32967393fd07bf9
Scott McInnis <scottalyst@gmail.com>
2019-11-27 13:49:30 -0800

    Adding styling and form input fields

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="mainContainer">
-                Bananas
+                <h1>Shinobi</h1>
                 <FunctionForm />
             </div>
         );
--- client/components/FunctionForm.jsx
+++ client/components/FunctionForm.jsx
@@ -3,14 +3,22 @@ import React from 'react';
 const FunctionForm = () => {
     return (
       <React.Fragment>
-        <input
-          type="text"
-          name="functionName"
-          placeholder="Function Name"
-        />
+        <input type="text" name="functionName" placeholder="Function Name" />
+        <select>
+          <option value="node8">Node 8</option>
+          <option value="node10">Node 10</option>
+          <option value="python37">Python 3.7</option>
+          <option value="go111">Go 1.11</option>
+          <option value="go113">Go 1.13</option>
+        </select>
         <pre>
-          <code contenteditable spellcheck="false">bananas</code>
+          <textarea placeholder="config" rows="10"></textarea>
         </pre>
+        <pre>
+          <textarea placeholder="<code here />" spellcheck="false" rows="25"></textarea>
+        </pre>
+
+        <button>Deploy</button>
       </React.Fragment>
     );
 };
