2c27929021f2e7838b8e9ceffef5162521795ad7
EngineerJustin <jrayjoseph@gmail.com>
2019-11-27 15:08:31 -0800

    Created a drag and drop or click interface to manage files

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,16 +3,18 @@ import React from 'react';
 import FunctionForm from './FunctionForm.jsx'
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {};
+    constructor(props) {
+        super(props);
+        this.state = {
+            uploadCode: ""
+        };
     }
 
     render() {
         return (
             <div className="mainContainer">
                 <h1>Shinobi</h1>
-                <FunctionForm />
+                <FunctionForm code={this.state.uploadCode} />
             </div>
         );
     }
