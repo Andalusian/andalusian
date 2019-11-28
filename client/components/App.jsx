c4ade6c73d7cf3d5aef6575e53ef69b8301c89e4
Talya Sasek <talyaercag@gmail.com>
2019-11-27 17:04:12 -0800

    Creates AWSFunctionForm and adds to App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,23 +1,44 @@
-import React from 'react';
-
-import FunctionForm from './FunctionForm.jsx'
+import React from "react";
+import FunctionForm from "./FunctionForm.jsx";
+import AWSFunctionForm from "./AWSFunctionForm.jsx";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            uploadCode: ""
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      uploadCode: "",
+      S3BucketName: "",
+      accessKey: "",
+      secretAccessKey: "",
+      region: "",
+      outputFormat: ""
+    };
+    this.updateInfo = this.updateInfo.bind(this);
+  }
 
-    render() {
-        return (
-            <div className="mainContainer">
-                <h1>Shinobi</h1>
-                <FunctionForm code={this.state.uploadCode} />
-            </div>
-        );
-    }
+  updateInfo(e) {
+    let updateObj = {};
+    updateObj[e.target.name] = e.target.value;
+    this.setState(updateObj);
+  }
+
+  render() {
+    return (
+      <div className="mainContainer">
+        <h1>Shinobi</h1>
+        <FunctionForm code={this.state.uploadCode} />
+        <h2>AWS</h2>
+        <AWSFunctionForm
+          S3BucketName={this.state.S3BucketName}
+          accessKey={this.state.accessKey}
+          secretAccessKey={this.state.secretAccessKey}
+          region={this.state.region}
+          outputFormat={this.state.outputFormat}
+          updateInfo={this.updateInfo}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
