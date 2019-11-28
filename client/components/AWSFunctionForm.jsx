f58ea345951f11ae05a8f1f01df7ede8970584b8
Talya Sasek <talyaercag@gmail.com>
2019-11-27 19:03:23 -0800

    Creates axios.post requests on AWSFunctionForm.jsx

--- client/components/AWSFunctionForm.jsx
+++ client/components/AWSFunctionForm.jsx
@@ -1,46 +1,50 @@
 import React from "react";
 import MyDropzone from "./MyDropzone.jsx";
+import axios from "axios";
 
 const AWSFunctionForm = props => {
-  // initialSetUp();
-
-  // function initialSetUp() {
-  //   // RUN THE TWO COMMANDS BELOW IN TERMINAL
-  //   // TO INSTALL AWS CLI
-  //   let initialSetUpCommand1 = `curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-macos.zip" -o "awscliv2.zip"`; //
-  //   let initialSetUpCommand2 = `unzip awscliv2.zip`; //
-  //   let initialSetUpCommand3 = `sudo ./aws/install`;
-  //   // TO INSTALL AWS SAM
-  //   let initialSetUpCommand4 = `brew tap aws/tap`;
-  //   let initialSetUpCommand5 = `brew install aws-sam-cli`;
-  // }
-
   function configure() {
-    // WE NEED TO GRAB THESE FROM THE INPUT FORM
-    let accessKey = props.accessKey;
-    let secretAccessKey = props.secretAccessKey;
-    let region = props.region;
-    let outputFormat = props.outputFormat;
-    // RUN THESE IN THE COMMAND LINE TO CONFIGURE AWS
-    let configureCommand1 = `aws2 configure`;
-    // ANSWER PROMPS USING THESE;
-    let configureCommand2 = accessKey;
-    let configureCommand3 = secretAccessKey;
-    let configureCommand4 = region;
-    let configureCommand5 = outputFormat;
+    // PASS THESE TO BACKEND TO RUN THESE IN THE COMMAND LINE TO CONFIGURE AWS
+    // let configureCommand1 = `aws2 configure set aws_access_key_id ${props.accessKey}`;
+    // let configureCommand2 = `aws2 configure set aws_secret_access_key ${props.secretAccessKey}`;
+    // let configureCommand3 = `aws2 configure set region ${props.region}`;
+    // let configureCommand4 = `aws2 configure set output ${props.outputFormat}`;
+
+    axios
+      .post("/aws/configure", {
+        accessKey: props.accessKey,
+        secretAccessKey: props.secretAccessKey,
+        region: props.region,
+        outputFormat: props.outputFormat
+      })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 
   function AWSDeploy() {
-    let bucketName = props.S3BucketName; // WE NEED TO GRAB THIS FROM THE INPUT FORM
-    // RUN THE TWO COMMANDS BELOW IN TERMINAL
-    let bucketNameCommand1 = `sam package \
-    --template-file template.yml \
-    --output-template-file package.yml \
-    --s3-bucket ${bucketName}`; //
-    let bucketNameCommand2 = `sam deploy \
-    --template-file package.yml \
-    --stack-name my-date-time-app \
-    --capabilities CAPABILITY_IAM`;
+    // PASS THESE TO BACKEND TO RUN THE TWO COMMANDS BELOW IN TERMINAL
+    // let bucketNameCommand1 = `sam package \
+    // --template-file template.yml \
+    // --output-template-file package.yml \
+    // --s3-bucket ${props.S3BucketName}`; //
+    // let bucketNameCommand2 = `sam deploy \
+    // --template-file package.yml \
+    // --stack-name my-date-time-app \
+    // --capabilities CAPABILITY_IAM`;
+    axios
+      .post("/aws/deploy", {
+        S3BucketName: props.S3BucketName
+      })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 
   return (
