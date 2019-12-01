0cab81713ad79f770978d84098c39d16fec94dcd
EngineerJustin <jrayjoseph@gmail.com>
2019-11-30 19:28:24 -0800

    Implemented conditional rendering based on a radio button selection

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FunctionForm from "./FunctionForm.jsx";
 import AWSFunctionForm from "./AWSFunctionForm.jsx";
+import MicroList from "./MicroList.jsx"
 
 class App extends React.Component {
   constructor(props) {
@@ -14,7 +15,8 @@ class App extends React.Component {
       region: "",
       outputFormat: "",
         fn_name: "",
-        runtime: undefined
+        runtime: undefined,
+      pageSelect: 'Gcloud'
     };
     this.updateInfo = this.updateInfo.bind(this);
   }
@@ -26,20 +28,45 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div className="mainContainer">
-        <h1>Shinobi</h1>
-        <FunctionForm runtime={this.state.runtime} fn_name={this.state.fn_name} uploadedKey={this.state.uploadedKey} updateInfo={this.updateInfo} code={this.state.uploadedFunction} />
-        <h2>AWS</h2>
-        <AWSFunctionForm
-            code={this.state.uploadedFunction}
+
+    let displayed;
+
+    if (this.state.pageSelect === 'Gcloud') {
+      displayed = <FunctionForm
+          runtime={this.state.runtime}
+          fn_name={this.state.fn_name}
+          uploadedKey={this.state.uploadedKey}
+          updateInfo={this.updateInfo}
+          code={this.state.uploadedFunction} />
+    } else if (this.state.pageSelect === 'Lambda') {
+      displayed = <AWSFunctionForm
+          code={this.state.uploadedFunction}
           S3BucketName={this.state.S3BucketName}
           accessKey={this.state.accessKey}
           secretAccessKey={this.state.secretAccessKey}
           region={this.state.region}
           outputFormat={this.state.outputFormat}
           updateInfo={this.updateInfo}
-        />
+      />
+    }
+
+    return (
+      <div className="mainContainer">
+        <h1>Shinobi</h1>
+        <MicroList/>
+        <div className='radio'>
+          <label>
+            <input onChange={() => this.updateInfo('pageSelect', 'Gcloud')} type="radio"
+                   value="Gcloud" checked={this.state.pageSelect === 'Gcloud'}/>
+            Gcloud
+          </label>
+          <label>
+            <input onChange={() => this.updateInfo('pageSelect', 'Lambda')} type="radio"
+                   value="Lambda" checked={this.state.pageSelect === 'Lambda'}/>
+            Lambda
+          </label>
+        </div>
+        {displayed}
       </div>
     );
   }
