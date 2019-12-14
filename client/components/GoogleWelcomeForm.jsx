476f71823a69f99f1219ca5daf8968e52169def0
Talya Sasek <talyaercag@gmail.com>
2019-12-14 10:20:43 -0800

    Started working on an account overview page and did styling

--- client/components/GoogleWelcomeForm.jsx
+++ client/components/GoogleWelcomeForm.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 
 const GoogleWelcomeForm = props => {
   return (
-    <form id="googleCredentials">
-      <pre>
-        <textarea id="googleKey" name="googleKey" onChange={(e) => props.updateInfo(e.target.name, e.target.value)} placeholder="Please add a Key File to Continue..." spellCheck="false" rows="10"></textarea>
-      </pre>
-      <label htmlFor="googleKeyAlias">Project Name:</label>
-      <input
-        type="text"
-        id="googleKeyAlias"
-        name="googleKeyAlias"
-        placeholder="Key Alias"
-        onChange={e => props.updateInfo(e.target.name, e.target.value)}
-      />
-      <button type="button" className="saveButton" onClick={() => props.submitKey('googleKey')}>Add New Key</button>
-    </form>
+    <div>
+      <h2>GCloud</h2>
+      <form id="googleCredentials">
+        <pre>
+          <textarea id="googleKey" name="googleKey" onChange={(e) => props.updateInfo(e.target.name, e.target.value)} placeholder="Please add a Key File to Continue..." spellCheck="false" rows="10"></textarea>
+        </pre>
+        <label htmlFor="googleKeyAlias">Project Name:</label>
+        <input
+          type="text"
+          id="googleKeyAlias"
+          name="googleKeyAlias"
+          placeholder="Key Alias"
+          onChange={e => props.updateInfo(e.target.name, e.target.value)}
+        />
+        <button type="button" className="saveButton" onClick={() => props.submitKey('googleKey')}>Add New Key</button>
+      </form>
+    </div>
   )
 }
 
