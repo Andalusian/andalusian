ffe102b4b97d732c1861f67050aa8ed301f20c24
Scott McInnis <scottalyst@gmail.com>
2019-12-18 16:57:35 -0800

    GoogleWelcomePage

--- client/components/GoogleWelcomeForm.jsx
+++ client/components/GoogleWelcomeForm.jsx
@@ -2,22 +2,27 @@ import React from 'react';
 
 const GoogleWelcomeForm = props => {
   return (
-    <div>
-      <h2>GCloud</h2>
-      <form id="googleCredentials">
-        <pre>
-          <textarea id="googleKey" name="googleKey" onChange={(e) => props.updateInfo(e.target.name, e.target.value)} placeholder="Please add a Key File to Continue..." spellCheck="false" rows="10"></textarea>
-        </pre>
-        <label htmlFor="googleKeyAlias">Project Name:</label>
-        <input
-          type="text"
-          id="googleKeyAlias"
-          name="googleKeyAlias"
-          placeholder="Key Alias"
-          onChange={e => props.updateInfo(e.target.name, e.target.value)}
-        />
-        <button type="button" className="saveButton" onClick={() => props.submitKey('googleKey')}>Add New Key</button>
-      </form>
+    <div id="googleGrid" className="grid">
+      <h2 className="container">GCloud</h2>
+      <div className="leftColumn">
+        <div id="googleCredentialsContainer" className="container">
+          <h3>Configuration</h3>
+          <form id="googleCredentials">
+            <pre>
+              <textarea id="googleKey" name="googleKey" onChange={(e) => props.updateInfo(e.target.name, e.target.value)} placeholder="Please add a Key File to Continue..." spellCheck="false" rows="10"></textarea>
+            </pre>
+            <label htmlFor="googleKeyAlias">Project Name:</label>
+            <input
+              type="text"
+              id="googleKeyAlias"
+              name="googleKeyAlias"
+              placeholder="Key Alias"
+              onChange={e => props.updateInfo(e.target.name, e.target.value)}
+            />
+            <button type="button" className="saveButton" onClick={() => props.submitKey('googleKey')}>Add New Key</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
