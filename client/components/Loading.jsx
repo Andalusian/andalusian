75315f685b24a1a7b9e211e24b548d0c70b6c989
Talya Sasek <talyaercag@gmail.com>
2019-12-18 16:26:18 -0800

    loading logo and docker page cleanup

--- client/components/Loading.jsx
+++ client/components/Loading.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Loading = props => {
 
     return (
-        <iframe id="loading" src="https://giphy.com/embed/feN0YJbVs0fwA" frameBorder="0" allowFullScreen></iframe>
+        <div id="loadingDiv">
+            <img id="loading" src="https://i.imgur.com/joQlpTb.gif" />
+        </div>
     );
 };
 
